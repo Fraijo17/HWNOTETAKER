a6964a064b63375986deb809fea973af2d9e63ac
Fraijo17 <abrahamfraijo17@gmail.com>
2021-02-27 10:49:59 -0700

    0

---
+++ routes/apiRoutes.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const util = require("util");
+const notesData = require("../db/db.json")
+const { v4:uuid } = require("uuid")
+const router = require("express").Router();
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+
+
+    router.get("/notes," function(req, res){
+        return readFileAsync("db/db.json", "utf8")
+        .then(function(data){
+            console.log(data)
+            data = JSON.parse(data);
+            data.push({...req.body, id:uuid()});
+
+            return writeFileAsync("db/db.json", JSON.stringify(data))
+            .then(function(){
+                console.log('random');
+                return res.json({ok:true})
+            })
+            .catch(function(err){
+                throw err
+            })
+        })
+    });
+
+
+    router.delete("/notes/:id", function(req, res){
+        return readFileAsync("db/db.json", "utf8")
+        .then(function(data){
+            const filteredNotes = JSON.parse(data).filter(note => note.id !== req.params.id);
+            console.log(filteredNotes);
+            return writeFileAsync("db/db.json", JSON.stringify(filteredNotes))
+            .then(function(){
+                console.log('random');
+                return res.json({ok:true})
+            })
+            .catch(function(err){
+                throw err
+            })
+        })
+    })
+
+    module.exports = router;
